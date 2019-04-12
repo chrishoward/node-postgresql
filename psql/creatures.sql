b8825b9ba0cd60efef9837219c23cf790321007b
Chris Howard <chrisau233@gmail.com>
2019-04-12 15:50:31 +1000

    complete sql, databases and postgresql section

--- psql/creatures.sql
+++ psql/creatures.sql
@@ -13,6 +13,16 @@ CREATE TABLE hobbits(
   personality character varying(50)
 );
 
+CREATE TABLE allies(
+  wizard character varying(50), 
+  elf character varying(50)
+);
+
+CREATE TABLE guardians(
+  elf character varying(50),
+  hobbit character varying(50)
+);
+
 INSERT INTO wizards(name,power)
 VALUES
 ('Gandalf', 'fireworks'),
@@ -30,3 +40,16 @@ VALUES
 ('Frodo', 'careful'),
 ('Sam', 'brave'),
 ('Bilbo', 'greedy');
+
+INSERT INTO allies(wizard,elf)
+VALUES
+('Gandalf', 'Legolas'),
+('Gandalf', 'Arwen'),
+('Saruman', 'Elrond'),
+('Saruman', 'Legolas');
+
+INSERT INTO guardians(elf,hobbit)
+VALUES
+('Legolas', 'Frodo'),
+('Arwen', 'Sam'),
+('Elrond', 'Bilbo');
