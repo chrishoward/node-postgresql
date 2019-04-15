2fc5b1dac6b22410deafd31e50298bc932261e53
Chris Howard <chrisau233@gmail.com>
2019-04-15 14:24:06 +1000

    initial commit

---
+++ monsters-api/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+
+app.get("/monsters", (request, response, next) => {
+  pool.query("SELECT * FROM monstersdb ORDER BY id ASC", (err, res) => {
+    if (err) return next(err);
+    response.json(res.rows);
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.json(err);
+});
+
+module.exports = app;
