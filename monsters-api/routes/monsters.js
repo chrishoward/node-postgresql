b4878c3b6883b0b5fbf4cbd590ebdc2b4437b315
Chris Howard <chrisau233@gmail.com>
2019-04-17 17:12:54 +1000

    add put and delete request handlers

--- monsters-api/routes/monsters.js
+++ monsters-api/routes/monsters.js
@@ -25,8 +25,37 @@ router.post("/", (request, response, next) => {
     [name, personality],
     (err, res) => {
       if (err) return next(err);
+
+      response.redirect("/monsters");
     }
   );
+});
+
+router.put("/:id", (request, response, next) => {
+  const { id } = request.params;
+  const keys = ["name", "personality"];
+  const fields = [];
+  keys.forEach(key => {
+    if (request.body[key]) fields.push(key);
+  });
+  fields.forEach((field, index) => {
+    pool.query(
+      `UPDATE monsters SET ${field}=($1) WHERE id=($2)`,
+      [request.body[field], id],
+      (err, res) => {
+        if (err) return next(err);
+        if (index === fields.length - 1) response.redirect("/monsters");
+      }
+    );
+  });
+});
+
+router.delete("/:id", (request, response, next) => {
+  const { id } = request.params;
+  pool.query("DELETE FROM monsters WHERE id=($1)", [id], (err, res) => {
+    if (err) return next(err);
+    response.redirect("/monsters");
+  });
 });
 
 module.exports = router;
